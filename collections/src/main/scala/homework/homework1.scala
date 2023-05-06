a49146f6dd6357d225c5fc853790b51859753bc5
aalekperov <alekperov.atash@gmail.com>
2023-05-06 23:50:29 +0300

    homework for lesson 2

--- collections/src/main/scala/homework/homework1.scala
+++ collections/src/main/scala/homework/homework1.scala
@@ -1,20 +1,36 @@
 package homework
 
 import scala.util.Random
-/*
+
 class BallsExperiment {
 
-  def isFirstBlackSecondWhite(): Boolean = {
-    ???
+  sealed trait Ball
+  case object BlackBall extends Ball
+  case object WhiteBall extends Ball
+
+  val ballsUrn: List[Ball] = List(WhiteBall, WhiteBall, WhiteBall, BlackBall, BlackBall, BlackBall)
+
+  def checkBall(ballsUrn: List[Ball], ball: Ball): (Boolean, List[Ball]) = {
+    val ballItem = Random.nextInt(ballsUrn.size)
+    val check = if (ballsUrn(ballItem) == ball) true else false
+    (check, ballsUrn.zipWithIndex.filter(_._2 != ballItem).map(_._1))
+  }
+
+  def isFirstBlackSecondWhite: Boolean = {
+    val checkBlackBall: (Boolean, List[Ball]) = checkBall(ballsUrn, BlackBall)
+    if (checkBlackBall._1) {
+      val isWhite = checkBall(checkBlackBall._2, WhiteBall)
+      isWhite._1
+    } else checkBlackBall._1
   }
 }
 
 object BallsTest {
   def main(args: Array[String]): Unit = {
-    val count = 10000
-    val listOfExperiments: List[BallsExperiment] = ???
-    val countOfExperiments = ???
+    val count = 1000000
+    val listOfExperiments: List[BallsExperiment] = List.fill(count)(new BallsExperiment)
+    val countOfExperiments = listOfExperiments.map(_.isFirstBlackSecondWhite)
     val countOfPositiveExperiments: Float = countOfExperiments.count(_ == true)
     println(countOfPositiveExperiments / count)
   }
-}*/
+}
